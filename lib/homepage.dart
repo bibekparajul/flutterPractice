4bebeab3f3ac81c2e6f80ac1143fac048ed0150a
Bibek Parajuli <parajulibbek11@gmail.com>
2022-09-23 21:29:17 +0545

    day2 completed

---
+++ lib/homepage.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+//ignore_for_file:prefer_const_constructors
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+    int days = 32;
+  String name = "Bbek";
+  double pi = 3.14;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+      title: Text("MyApp"),
+    ),
+    body: Center(
+      child: Container(
+        child: Text("Welcome to the $days of flutter by $name", style: TextStyle(fontWeight: FontWeight.bold),),
+      ),
+    ),
+    );
+  }
+}
