d97bae6ccfd17ae77e3ebc707c24a79a0bce3876
Bibek Parajuli <parajulibbek11@gmail.com>
2022-10-04 09:03:05 +0545

    badge,http,api,networking

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -2,8 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'dart:convert';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+
+import 'package:flutter_catalog/core/store.dart';
+import 'package:flutter_catalog/models/cart.dart';
 import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/utils/routes.dart';
 import 'package:flutter_catalog/widgets/themes.dart';
@@ -44,24 +45,34 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+
+    final _cart = (VxState.store as MyStore).cart;
     return Scaffold(
       backgroundColor: context.canvasColor,
-      floatingActionButton: FloatingActionButton(onPressed: ()=> Navigator.pushNamed(context, MyRoutes.cartRoute),
-      child: Icon(CupertinoIcons.cart, color: Colors.white,),
-      
-      backgroundColor: context.theme.buttonColor,),
+      floatingActionButton: VxBuilder(
+        mutations: {AddMutation,RemoveMutation},
+        builder:(context,store,_) => FloatingActionButton(
+          onPressed: () => Navigator.pushNamed(context, MyRoutes.cartRoute),
+          child: Icon(
+            CupertinoIcons.cart,
+            color: Colors.white,
+          ),
+          backgroundColor: context.theme.buttonColor,
+        ).badge(color: Vx.green400, size: 20, count: _cart.items.length,
+         textStyle: TextStyle(color: Colors.black, fontWeight: FontWeight.bold)
+         ),
+      ),
       body: SafeArea(
         child: Container(
-          padding: Vx.m20,
+          padding: Vx.m16,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               CatalogHeader(),
               if (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
-                CatalogList().py16().expand()
+                CatalogList().py8().expand()
               else
-                  CircularProgressIndicator().centered().expand(),
-                
+                CircularProgressIndicator().centered().expand(),
             ],
           ),
         ),
@@ -69,4 +80,3 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
-
